7d33a7afe5e68742dd3127d2ddc4366882aeacba
Subhasis Ray <ray.subhasis@gmail.com>
2011-11-03 16:07:08 +0530

    Did some testing on nitime based filtering  and spectral plots.

--- nitime_filter_test.py
+++ nitime_filter_test.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Sat Oct 29 22:19:59 2011 (+0530)
 # Version: 
-# Last-Updated: Sat Oct 29 23:12:43 2011 (+0530)
+# Last-Updated: Wed Nov  2 23:13:01 2011 (+0530)
 #           By: Subhasis Ray
-#     Update #: 63
+#     Update #: 115
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -33,51 +33,65 @@ import h5py
 import nitime
 from nitime.timeseries import TimeSeries
 from nitime.analysis import FilterAnalyzer, SpectralAnalyzer, NormalizationAnalyzer
-
+from analyzer import blackmann_windowedsinc_filter
 filename = '/media/sda6/cortical_data/2011_10_21/data_20111021_143831_21310.h5'
 fhandle = h5py.File(filename, 'r')
-datanode = fhandle['/lfp/electrode_1000um']
-data = numpy.zeros(3999)
-data[:] = datanode[1:4000] # first entry is 0.0 causing errors in analysis
 sampling_interval = fhandle.attrs['plotdt']
+stim_dur = 0.5
+sample_count = int(stim_dur/sampling_interval+0.5)
+stim_start = 3.5
+start_index = int(stim_start / sampling_interval + 0.5)
+datanode = fhandle['/lfp/electrode_1000um']
+data = numpy.zeros(2000)
+data[:] = datanode[start_index:start_index+sample_count] # first entry is 0.0 causing errors in analysis
+data = blackmann_windowedsinc_filter([data], sampling_interval)
+
 # numpy.savetxt('electrode1mm.dat', data)
 def test_filtering(data, sampling_interval):
     print 'sampling interval = ', sampling_interval
     T = TimeSeries([data], sampling_interval=sampling_interval)
     S_original = SpectralAnalyzer(T)
-    fig01 = pylab.figure()
-    
-    ax01 = fig01.add_subplot(5, 1, 1)
-    ax01.plot(S_original.psd[0], S_original.psd[1][0], label='Welch PSD')
-    ax02 = fig01.add_subplot(5, 1, 2)
-    ax02.plot(S_original.spectrum_fourier[0],
-              S_original.spectrum_fourier[1][0], label='FFT')
-    ax03 = fig01.add_subplot(5, 1, 3)
-    ax03.plot(S_original.periodogram[0],
-              S_original.periodogram[1][0], label='Periodogram')
-    ax04 = fig01.add_subplot(5, 1, 4)
-    ax04.plot(S_original.spectrum_multi_taper[0],
-              S_original.spectrum_multi_taper[1][0], label='Multi_taper')
+    pylab.figure()
+    print len(S_original.psd[0]), len(S_original.psd[1][0])
+    pylab.plot(S_original.psd[0], S_original.psd[1][0], 'r.', label='Welch PSD')
+    pylab.legend()
+
+    pylab.figure()
+    pylab.plot(S_original.spectrum_fourier[0],
+              S_original.spectrum_fourier[1][0], 'r.', label='FFT')
+    pylab.legend()
+
+    pylab.figure()
+    pylab.plot(S_original.periodogram[0],
+              S_original.periodogram[1][0], 'r.', label='Periodogram')
+    pylab.legend()
+
+    pylab.figure()
+    pylab.plot(S_original.spectrum_multi_taper[0],
+              S_original.spectrum_multi_taper[1][0], 'r.', label='Multi_taper')
+    pylab.legend()
+
     F = FilterAnalyzer(T, ub=400, lb=1.0)
-    ax05 = fig01.add_subplot(5, 1, 5)
-    ax05.plot(F.data[0], label='Unfiltered')
+    pylab.figure()
+    pylab.plot(F.data[0], label='Unfiltered')
+    pylab.legend()
     # ax01.set_xlabel('Frequency (Hz)')
     # ax01.set_ylabel('Power')    
-    ax01.legend( )   
-    ax02.legend( )   
-    ax03.legend( )   
-    ax04.legend( )
-    ax05.legend()
-    fig02 = pylab.figure()
-    ax11 = fig02.add_subplot(1, 1, 1)
-    ax11.plot(F.filtered_boxcar.data[0], label='Boxcar filter')
-    ax11.plot(F.fir.data[0], label='FIR')
-    ax11.plot(F.iir.data[0], label='IIR')
-    ax11.plot(F.filtered_fourier.data[0], label='Fourier filter')
-    ax11.legend()
+    # ax01.legend( )   
+    # ax02.legend( )   
+    # ax03.legend( )   
+    # ax04.legend( )
+    # ax05.legend()
+    # fig02 = pylab.figure()
+    # ax11 = fig02.add_subplot(1, 1, 1)
+    # ax11.plot(F.filtered_boxcar.data[0], label='Boxcar filter')
+    # ax11.plot(F.fir.data[0], label='FIR')
+    # ax11.plot(F.iir.data[0], label='IIR')
+    # ax11.plot(F.filtered_fourier.data[0], label='Fourier filter')
+    # ax11.legend()
     pylab.show()
 
 if __name__ == '__main__':
-    test_filtering(data, sampling_interval)
+    test_filtering(data[0], sampling_interval)
 # 
 # test.py ends here
