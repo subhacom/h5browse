c34d679f1003c18c70eaab4de354bab2be51032c
Subhasis Ray <ray.subhasis@gmail.com>
2011-12-28 10:09:44 +0530

    Updated connfinder to use main HDF5 API for reading data. (just to get a taste of how difficult it could be).

--- c/connfinder.cpp
+++ c/connfinder.cpp
@@ -6,9 +6,9 @@
 // Maintainer: 
 // Created: Mon Dec 26 15:06:27 2011 (+0530)
 // Version: 
-// Last-Updated: Tue Dec 27 17:28:00 2011 (+0530)
-//           By: subha
-//     Update #: 212
+// Last-Updated: Tue Dec 27 22:35:17 2011 (+0530)
+//           By: Subhasis Ray
+//     Update #: 229
 // URL: 
 // Keywords: 
 // Compatibility: 
@@ -48,35 +48,6 @@
 
 using namespace std;
 
-
-// herr_t collect_spikedata(hid_t loc_id, const char * name, const H5L_info_t * info, void * operator_data)
-// {
-//     herr_t status;
-//     H5O_info_t infobuf;
-//     status = H5O_get_info_by_name(loc_id, name, &infobuf, H5P_DEFAULT);
-//     if (infobuf.type == H5O_TYPE_DATASET){
-//         spike_names[COUNT] = (char*)calloc(sizeof(char), strlen(name));
-//         strncpy(spike_names[COUNT], name, VSNAMELENMAX);
-//         int length = 0;
-//         status = H5LTget_dataset_ndims(loc_id, name, &length);
-//         if (status < 0){
-//             cerr << "Error getting dataset dims for " << name << ". H5LTget_dataset_ndims returned : " << status << endl;
-//             free(spike_names[COUNT]);
-//             return status;
-//         }
-//         operator_data = (double*)calloc(sizeof(double), (size_t)length);
-//         status = H5LTread_dataset_double(loc_id, name, spike_data[COUNT]);
-//         if (status < 0){
-//             cerr << "Error reading dataset for " << name << ". H5LTread_dataset_ndims returned : " << status << endl;
-//             free(spike_names[COUNT]);
-//             free(spike_data[COUNT]);
-//             return status;
-//         }        
-//     }
-//     return 0;
-// }
-
-
 herr_t spike_data_op(hid_t loc_id, const char * name, const H5L_info_t * info, void * operator_data)
 {
     vector< pair< string, vector<double> > > * data_list = (vector < pair < string, vector < double > > >*)operator_data;
@@ -92,18 +63,23 @@ herr_t spike_data_op(hid_t loc_id, const char * name, const H5L_info_t * info, v
         vector<double> data;
         hid_t dset_id = H5Dopen(loc_id, name);
         hid_t filespace_id = H5Dget_space(dset_id);
-        hid_t memspace_id;
         int rank = H5Sget_simple_extent_ndims(filespace_id);
         cout << name << ", rank: " << rank << endl;
         hsize_t dims[2];
         status = H5Sget_simple_extent_dims(filespace_id, dims, NULL);
-        cout << "Dataset size is: " << size << endl;
+        cout << "Dataset size is: " << dims[0] << endl;
         // This is unsafe, I should check that double is same size as HDF5 double.
-        data.resize(size);
-        hsize_t dims[] = {size};
-        hid_t memtype_id = H5Tarray_create(H5T_NATIVE_DOUBLE, 1, dims, 0);
-        // status = H5Dread(dset_id, memtype_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data[0]);
-        status = H5LTread_dataset_double(loc_id, name, &data[0]);
+        data.resize(dims[0]);
+        hid_t props = H5Dget_create_plist(dset_id);
+        if (H5D_CHUNKED == H5Pget_layout(props)){
+            hsize_t chunk_dims[2];
+            int rank_chunk = H5Pget_chunk(props, 2, chunk_dims);
+            cout << "chunk rank " << rank_chunk << ", dimensions " << chunk_dims[0] << "x" << chunk_dims[1] << endl;
+        }
+        hid_t memspace_id = H5Screate_simple(1, dims, NULL);
+        status = H5Dread(dset_id, H5T_NATIVE_DOUBLE, memspace_id, filespace_id, H5P_DEFAULT, &data[0]);
+        // status = H5LTread_dataset_double(loc_id, name, &data[0]);
+        H5Dclose(dset_id);
         data_list->push_back(pair < string, vector< double > > (string(name), data));
     }
     return 0;
