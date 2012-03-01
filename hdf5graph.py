938826b06276112ea18bd9fb2df741cfab7e3cd9
Subhasis <ray.subhasis@gmail.com>
2012-03-01 20:20:54 +0530

    Updated hdf5graph with faster and cleaner implementation.

--- hdf5graph.py
+++ hdf5graph.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Wed Feb 29 16:25:03 2012 (+0530)
 # Version: 
-# Last-Updated: Wed Feb 29 18:54:18 2012 (+0530)
+# Last-Updated: Thu Mar  1 19:54:52 2012 (+0530)
 #           By: subha
-#     Update #: 112
+#     Update #: 195
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -29,67 +29,60 @@
 # 
 
 # Code:
-
+import sys
 import igraph as ig
 import h5py as h5
 import numpy
 
 class HDFGraph(object):
     def __init__(self, filename):
-        fileh = h5.File(filename, 'r')
-        self.sources = []
-        self.dests = []
-        self.weights = []
-        self.nodes = set()
-        self.name_node = {}
-        self.node_name = {}
-        synrows = numpy.asarray(fileh['/network/synapse'])
-        src_index_dict = {}
-        dest_index_dict = {}
-        for ii in range(len(synrows)):
-            row = synrows[ii]
-            source = row[0].partition('/')[0]
-            dest = row[1].partition('/')[0]
-            weight = float(row[3])
-            src_index_dict[source] = ii
-            dest_index_dict[dest] = ii
-            self.sources.append(source)
-            self.dests.append(dest)
-            self.weights.append(weight)
-        
-        
-        #     self.sources.append()
-        #     self.dests.append()
-        #     self.weights.append()
-        src_set = set(self.sources)
-        dest_set = set(self.dests)
-        node_set = src_set | dest_set
-        index = 0
-        for nname in node_set:
-            self.name_node[nname] = index
-            self.node_name[index] = nname
-            index += 1
-        self.graph = ig.Graph(0,directed=True)
-        self.graph.add_vertices(len(node_set))
-        edges = [(self.name_node[self.sources[ii]], self.name_node[self.dests[ii]]) for ii in range(len(self.sources))]
-        self.graph.add_edges(edges)
-        self.graph.vs['label'] = [self.node_name[node] for node in range(len(self.node_name))]
-        self.graph.es['weight'] = self.weights
-        # for ii in range(len(self.weights)):
-        #     src_index = self.name_node[self.sources[ii]]
-        #     dest_index = self.name_node[self.dest[ii]]
-        #     self.graph.es
-        
-        # self.graph.es['weight'] = [
-        # # for ii in range(len(self.sources)):
-        # #     self.graph.es[ii]['weight'] = self.weights[ii]
-        # for node, name in self.node_name.items():
-        #     self.graph.vs[node]['label'] = name
-        
-        
+        self.filename = filename
+        filehandle = h5.File(filename, 'r')
+        self.syntab = numpy.asarray(filehandle['/network/synapse'])
+        self.graph = ig.Graph(0, directed=True)
+        src = [row[0].rpartition('/')[0] for row in self.syntab]
+        dst = [row[1].rpartition('/')[0] for row in self.syntab]
+        weight = [row[3] for row in self.syntab]
+        nodeset = set(src) | set(dst)
+        nodeindex = dict(zip(nodeset, range(len(nodeset))))
+        self.graph.add_vertices(len(nodeset))
+        self.graph.add_edges([(nodeindex[src[ii]], 
+                               nodeindex[dst[ii]]) 
+                              for ii in range(len(src))])
+        self.graph.es['weight'] = weight
+        self.graph.vs['label'] = list(nodeset)
+
+    def write_dot(self, filename):
+        self.graph.write_dot(filename)
+
+    def write_gml(self, filename):
+        self.graph.write_gml(filename)
+
+    def get_vclustering(self, membership=None, modularity=None):
+        return ig.VertexClustering(self.graph, 
+                                   membership=membership, 
+                                   modularity=modularity)
+
 if __name__ == '__main__':
-    g = HDFGraph('../py/data/2012_02_01/network_20120201_204744_29839.h5.new')
-    ig.plot(g.graph)
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = "../py/data/2012_02_08/network_20120208_115556_4589.h5.new"
+    g = HDFGraph(filename)
+    dot_file = g.filename.replace('.h5', '').replace('new', 'dot')
+    gml_file = g.filename.replace('.h5', '').replace('new', 'gml')
+    g.write_gml(gml_file)
+    g.write_dot(dot_file) 
+    clusters = g.graph.clusters()
+    print clusters.size_histogram()
+    for cluster in clusters:
+        print cluster
+    # membership = g.clusters()
+    # clustering = g.get_vclustering(membership=g.graph.clusters().membership)
+    # for ii in range(len(clustering)):
+    #     cluster = clustering.subgraph(ii)
+    #     print len(cluster)
+    #     # ig.plot(cluster)
 
 # 
 # hdf5graph.py ends here
