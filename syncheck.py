3300a0a25dcccfcf48b3545c3064d86fc387f590
Subhasis <ray.subhasis@gmail.com>
2012-04-27 15:50:57 +0530

    Cleaned up syncheck.

--- syncheck.py
+++ syncheck.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Wed Apr 25 10:45:32 2012 (+0530)
 # Version: 
-# Last-Updated: Thu Apr 26 20:10:58 2012 (+0530)
+# Last-Updated: Fri Apr 27 13:23:55 2012 (+0530)
 #           By: subha
-#     Update #: 175
+#     Update #: 443
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -50,94 +50,159 @@ celltype_color_dict = {
 def normalize(data):
     if max(data) == min(data):
         return data
-    return min(data) + data/(max(data) - min(data))
+    return (data  - min(data))/(max(data) - min(data))
 
-def get_presynaptic(syntab, cellname, srctype, syntype):
-    """Return a view containing the presynaptic cells of `cellname` of
-    type `srctype` with synapses of `syntype`"""
-    idx = np.char.startswith(syntab['dest'], cellname) & \
-        np.char.startswith(syntab['source'], srctype) & \
-        np.char.equal(syntab['type'], syntype)
-    return syntab[idx]
+class SynAnalyzer(object):
+    def __init__(self, datafilepath):
+        netfilepath = datafilepath.replace('/data_', '/network_') + '.new'
+        self.datafile = h5.File(datafilepath, 'r')
+        self.netfile = h5.File(netfilepath, 'r')
+        self.simdt = 1.0
+        self.plotdt = 1.0
+        self.simtime = 10.0
+        for row in self.datafile['runconfig/scheduling'][:]:
+            if row[0] == 'simdt':
+                self.simdt = float(row[1])
+            elif row[0] == 'plotdt':
+                self.plotdt = float(row[1])
+            elif row[0] == 'simtime':
+                self.simtime = float(row[1])
+        self.syntab = self.netfile['/network/synapse']
+        self.istart = 0
+        self.iend = -1
 
-def plot_presynaptic(cellname, srctype, syntype, netfile, datafile, tstart, tend):
-    presynaptic = get_presynaptic(netfile['/network/synapse'][:], cellname, srctype, syntype)
-    precells = [item[0] for item in np.char.split(presynaptic['source'], '/')]
-    plotdt = datafile.attrs['plotdt']
-    simdt = datafile.attrs['simdt']
-    istart = int(tstart/plotdt)
-    iend = int(tend/plotdt)
-    for ii in range(len(precells)):
-        print 'Presynaptic cell:', precells[ii]
-        pretype = precells[ii].split('_')[0]
-        syndata_path = 'gk_%s_%s_from_%s' % (presynaptic['dest'][ii].replace('/', '_'),
-                                             syntype,
-                                             pretype)
+    def __del__(self):
+        self.netfile.close()
+        self.datafile.close()
+
+    def select_syninfo(self, cellname, srctype, syntype):
+        """Return a view containing the presynaptic cells of `cellname` of
+        type `srctype` with synapses of `syntype`"""
+        idx = np.char.startswith(self.syntab['dest'], cellname) & \
+            np.char.startswith(self.syntab['source'], srctype) & \
+            np.char.startswith(self.syntab['type'], syntype)
+        return self.syntab[idx]
+
+    def get_normalized_gk_slice(self, postcell, pretype, syntype):
+        gkpath = 'gk_%s_%s_from_%s' % (postcell, syntype, pretype)
+        gk = []
         try:
-            gk = np.asarray(datafile['/synapse/' + syndata_path ])
-            gk = normalize(gk)[istart:iend]
-            # Shift the normalize plots around 0 so they don't overlap too much with the original
-            l2d = plt.plot(np.linspace(tstart, tend, len(gk)), 
-                     gk + ii - len(precells)/2, 
-                     # color=celltype_color_dict[pretype], 
-                     ls='-.',
-                     label=syndata_path)
-            vm = np.asarray(datafile['/Vm/'+ precells[ii]])
-            vm = normalize(vm)[istart:iend]
-            plt.plot(np.linspace(tstart, tend, len(vm)), 
-                     vm + ii - len(precells)/2, 
-                     color = l2d[0].get_color(),
-                     # color=celltype_color_dict[pretype],
-                     label=precells[ii])
+            gk = np.asarray(self.datafile['/synapse/'+gkpath])
+            gk = normalize(gk)[self.istart:self.iend]
         except KeyError:
-            print 'No synaptic Gk for', cellname, '<-', pretype
+            print 'No synaptic Gk for', gkpath
+        return gk
+    
+    def plot_presynaptic(self, cellname, 
+                         srctype, 
+                         syntype):
+        syntab = self.select_syninfo(cellname, srctype, syntype)
+        print syntab
+        precells = [item[0] for item in np.char.split(syntab['source'], '/')]
+        unique_precells = list(set(precells))
+        syntypes = ['gaba', 'ampa', 'nmda']
+        lstyles = {'gaba':'-.', 'ampa':'--', 'nmda':':'}
+        # If specified `syntype` is not empty string, it is the only one
+        # to be looked at
+        for ii in range(len(unique_precells)):
+            print 'Presynaptic cell:', precells[ii]
+            pretype = unique_precells[ii].split('_')[0]
+            try:
+                vm = np.asarray(self.datafile['/Vm/'+ unique_precells[ii]])
+                print min(vm), max(vm)
+                vm = normalize(vm)[self.istart:self.iend]
+                print min(vm), max(vm)
+                # Shift the normalize plots around 0 so all of them don't
+                # overlap with the original
+                l2d = plt.plot(self.tseries, 
+                               vm + ii + 1,
+                               label=unique_precells[ii])
+            except KeyError:
+                print 'No Vm for', unique_precells[ii]
+                continue            
+            try:
+                jj = -1
+                while True:
+                    jj = precells.index(unique_precells[ii], jj+1)
+                    gk = self.get_normalized_gk_slice(
+                        syntab['dest'][jj].replace('/', '_'),
+                        pretype,
+                        syntab['type'][jj])
+                    if len(gk) == 0:
+                        continue
+                    plt.plot(self.tseries,
+                             gk + ii + 1,
+                             color=l2d[0].get_color(),
+                             ls=lstyles[syntab['type'][jj]],
+                             label='%s:%s<-%s' % (syntab['type'][jj], 
+                                                  syntab['dest'][jj].split('/')[0], 
+                                                  pretype))
+            except ValueError:
+                pass
 
-def test_conn(filename):
-    h5f = h5.File(filename, 'r')
-    syntab = h5f['network']['synapse'][:]
-    src = np.char.split(syntab['source'], '/')
-    dest = np.char.split(syntab['dest'], '/')
-    src = [item[0] for item in src]
-    dest = [item[0] for item in dest]
-    error = False
-    for ii in range(len(src)):
-        if src[ii] == dest[ii]:
-            print 'Self connection:', syntab[ii]
-            error = True
-    if error:
-        print 'Self connections found in', filename
-    h5f.close()
+    def plot_traces(self, cellname, targettime, historytime, srctype, syntype):
+        """Plot traces of presynaptic data for cellname for `historytime`
+        around `targettime`."""
+        self.tstart = targettime - historytime
+        self.istart = int(self.tstart / self.plotdt + 0.5)
+        self.tend = targettime + historytime
+        self.iend = int(self.tend / self.plotdt + 0.5)
+        self.tseries = np.linspace(self.tstart, self.tend, 
+                                 self.iend - self.istart)
+        vm = self.datafile['/Vm/' + cellname]        
+        plt.plot(self.tseries, 
+                 normalize(vm[self.istart:self.iend]),
+                 ls=':',
+                 label=cellname)
+        self.plot_presynaptic(cellname, srctype, syntype)
+        stimdata = np.asarray(self.datafile['/stimulus/stim_bg'])
+        stim_start = int(self.tstart/self.simdt+0.5)
+        stim_end = int(self.tend/self.simdt+0.5)
+        stimdata = stimdata[stim_start: stim_end]
+        plt.plot(np.linspace(self.tstart, self.tend, len(stimdata)),
+                 normalize(stimdata),
+                 'r--',                 
+                 label='STIMULUS')
+
 
 if __name__ == '__main__':
     dfname = '/data/subha/cortical/py/data/2012_04_24/data_20120424_145719_7507.h5'
-    nfname = '/data/subha/cortical/py/data/2012_04_24/network_20120424_145719_7507.h5.new'
-    cellname = 'SpinyStellate_21'
-    sourcetype = ''#'TCR'
-    synapsetype = 'nmda'
-    df = h5.File(dfname, 'r')
-    nf = h5.File(nfname, 'r')
-    plotdt = 1.0
-    simdt = 1.0
-    simtime = 10.0
-    for row in df['runconfig/scheduling'][:]:
-        if row[0] == 'simdt':
-            simdt = float(row[1])
-        elif row[0] == 'plotdt':
-            plotdt = float(row[1])
-        elif row[0] == 'simtime':
-            simtime = float(row[1])
-    vm = df['/Vm/'+cellname][:]
-    vm = normalize(vm)
-    plt.plot(np.linspace(0, simtime, len(vm)), 
-             vm, 
-             color=celltype_color_dict[cellname.split('_')[0]], 
-             ls=':',
-             label=cellname)
-    tstart = 6.0
-    tend = 7.5
-    plot_presynaptic(cellname, sourcetype, synapsetype, nf, df, tstart, tend)
-    plt.legend(loc='lower left')
+    synan = SynAnalyzer(dfname)
+    synan.plot_traces('SpinyStellate_0', 
+                      7.0,
+                      0.05,
+                      '',
+                      '')
+    plt.legend()
     plt.show()
+    # nfname = '/data/subha/cortical/py/data/2012_04_24/network_20120424_145719_7507.h5.new'
+    # cellname = 'SpinyStellate_21'
+    # sourcetype = ''#'TCR'
+    # synapsetype = 'nmda'
+    # df = h5.File(dfname, 'r')
+    # nf = h5.File(nfname, 'r')
+    # plotdt = 1.0
+    # simdt = 1.0
+    # simtime = 10.0
+    # for row in df['runconfig/scheduling'][:]:
+    #     if row[0] == 'simdt':
+    #         simdt = float(row[1])
+    #     elif row[0] == 'plotdt':
+    #         plotdt = float(row[1])
+    #     elif row[0] == 'simtime':
+    #         simtime = float(row[1])
+    # vm = df['/Vm/'+cellname][:]
+    # vm = normalize(vm)
+    # plt.plot(np.linspace(0, simtime, len(vm)), 
+    #          vm, 
+    #          color=celltype_color_dict[cellname.split('_')[0]], 
+    #          ls=':',
+    #          label=cellname)
+    # tstart = 6.0
+    # tend = 7.5
+    # plot_presynaptic(cellname, sourcetype, synapsetype, nf, df, tstart, tend)
+    # plt.legend(loc='lower left')
+    # plt.show()
 
 # 
 # syncheck.py ends here
