f1c69641afac096e6adac700aa7c1d5946d14109
Subhasis <ray.subhasis@gmail.com>
2012-12-04 19:37:11 +0530

    Working version of script to analyze non-bursting spiny stellate. Plotting gE and gI for SpinyStellate_0 from all such available files killed chamcham. Last I checked it was using 3.2 G RAM. The stimulus needs to be scaled to the same range as the conductance when plotting.

--- analyze_nonbursting.py
+++ analyze_nonbursting.py
@@ -7,9 +7,9 @@
 # Maintainer: 
 # Created: Tue Dec  4 16:40:12 2012 (+0530)
 # Version: 
-# Last-Updated: Tue Dec  4 17:01:54 2012 (+0530)
+# Last-Updated: Tue Dec  4 19:44:53 2012 (+0530)
 #           By: subha
-#     Update #: 5
+#     Update #: 117
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -43,11 +43,76 @@ from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import pyplot as plt
 from matplotlib._pylab_helpers import Gcf
 import random
-
+from traubdata import TraubData
 import analyzer
 
+def load_nonbursting_spinystellate_cell_file(fromfile='nonbursting_spinystellates.txt'):
+    filename_cell_map = defaultdict(set)
+    cell_filename_map = defaultdict(set)
+    with open(fromfile) as flistfile:
+        for line in flistfile:
+            stripped = line.strip()
+            if (not stripped) or stripped.startswith('#'):
+                continue
+            tokens = [t.strip() for t in stripped.split(',')]
+            filename_cell_map[tokens[0]].add(tokens[1])
+            cell_filename_map[tokens[1]].add(tokens[0])
+    return (filename_cell_map, cell_filename_map)
+
+def load_nonbursting_spinystellate_frac_file(fromfile='nonbursting_spinystellate_frac.txt'):
+    filename_frac_map = defaultdict(list)
+    with open(fromfile) as flistfile:
+        for line in flistfile:
+            stripped = line.strip()
+            if (not stripped) or stripped.startswith('#'):
+                continue
+            tokens = [t.strip() for t in stripped.split(',')]
+            filename_frac_map[tokens[0]].append(float(tokens[1]))
+    return filename_frac_map
+    
+def plot_synaptic_conductances(cell_filename_map, filename_data_map):
+    """Plot the excitatory and inhibitory conductances for all cells
+    for which the synaptic conductance information is available
+    (practically 'SpinyStellate_0' in most recent simulations). 
+
+    cell_filename_map: dict mapping cellnames to filenames.
+
+    filename_data_map: dict mapping filenames to TraubData objects.
+
+    """
+    rows = 4
+    all_rows = len(cell_filename_map['SpinyStellate_0'])    
+    fig = None
+    axno = 0
+    filenames = sorted(cell_filename_map['SpinyStellate_0'])
+    for filename in filenames:
+        data = filename_data_map[filename]
+        if not 'synapse' in data.fdata:
+            print 'No synaptic conductance in', data.fdata.filename
+            continue
+        gE = np.sum([np.asarray(data.fdata['synapse'][gsyn]) for gsyn in data.fdata['synapse'] if ('ampa' in gsyn) or ('nmda' in gsyn)],
+                         axis=0)
+        gI = np.sum([np.asarray(data.fdata['synapse'][gsyn]) for gsyn in data.fdata['synapse'] if 'gaba' in gsyn],
+                         axis=0)
+        ts = np.linspace(0, data.simtime, len(gE))
+        if axno % rows == 0:
+            fig = plt.figure()
+        ax = fig.add_subplot(rows, 1, axno%rows+1)
+        ax.set_title(filename)
+        ax.set_ylabel('Conductance (nS)')
+        cellcounts = '\n'.join(['%s:%d' % item for item in data.cellcounts._asdict().items()])
+        ax.text(2, 5, cellcounts, ha='left')
+        ax.plot(ts, gE*1e9, 'r-', alpha=0.7)
+        ax.plot(ts, gI*1e9, 'b-', alpha=0.3)
+        ts = np.linspace(0, data.simtime, len(data.bg_stimulus))
+        ax.plot(ts, data.bg_stimulus, 'g-.', alpha=0.5)
+        axno += 1
+
 if __name__ == '__main__':
-    pass
+    filename_cell_map, cell_filename_map = load_nonbursting_spinystellate_cell_file()
+    filename_data_map = dict([(filename, TraubData(filename)) for filename in filename_cell_map.keys()])
+    plot_synaptic_conductances(cell_filename_map, filename_data_map)
+    plt.show()
 
 
 # 
