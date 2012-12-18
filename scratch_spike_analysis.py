0e2b842123d9e7646aa4ae236565362597a9f556
Subhasis <ray.subhasis@gmail.com>
2012-12-18 20:26:49 +0530

    Added testing code to save the cell-graph in graphml/pajek formats for investigation with graph analysis software (like gephi). Have not found anythin interesting in the odd spiny stellate cells yet.

--- scratch_spike_analysis.py
+++ scratch_spike_analysis.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Wed Dec 12 11:43:23 2012 (+0530)
 # Version: 
-# Last-Updated: Tue Dec 18 18:58:33 2012 (+0530)
+# Last-Updated: Tue Dec 18 20:22:19 2012 (+0530)
 #           By: subha
-#     Update #: 244
+#     Update #: 264
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -31,6 +31,7 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.colors import ColorConverter
 import igraph as ig
 from traubdata import TraubData
 import peakdetect as pdet
@@ -47,6 +48,7 @@ if __name__ == '__main__':
     pre_cells = [row[0] for row in pre_cells]
     post_cells = np.char.split(synapses['dest'], '/')
     post_cells = [row[0] for row in post_cells]
+    gk = []    
     cells = sorted(set(pre_cells).union(set(post_cells)))
     cell_index_map = dict([(cell, index) for index, cell in enumerate(cells)])
     print cell_index_map
@@ -64,15 +66,26 @@ if __name__ == '__main__':
     g.vs['color'] = [colordict[celltype] for celltype in g.vs['celltype']]
     # ig.plot(g)
     g1 = nx.MultiDiGraph()
-    g1.add_edges_from(zip(pre_cells, post_cells))
+    edges = [(row['source'], row['dest'], {'weight': float(row['Gbar'])}) for row in synapses]
+    g1.add_edges_from(edges)
     node_colors = []
     for node in g1.nodes():
         if node in cats['odd_cells']:
             node_colors.append('cyan')
         else:
             node_colors.append(colordict[node.split('_')[0]])
-    nx.draw_spectral(g1, node_color=node_colors, alpha=0.5, edge_color=[(0.5,0.5,0.5, 0.2)] * len(g1.edges()), with_labels=False)
-    plt.show()
+    color_conv = ColorConverter()
+    for index, node in enumerate(g1.nodes()):
+        r, g, b = color_conv.to_rgb(node_colors[index])
+        g1.node[node]['r'] = int(r*255)
+        g1.node[node]['g'] = int(g*255)
+        g1.node[node]['b'] = int(b*255)
+                
+        print g1.node[node]
+    nx.write_pajek(g1, 'cellgraph.net')
+    nx.write_graphml(g1, 'cellnet.graphml')
+    # nx.draw_graphviz(g1, node_color=node_colors, alpha=0.5, edge_color=[(0.5,0.5,0.5, 0.2)] * len(g1.edges()), with_labels=False)
+    # plt.show()
     # combined_spikes = []
     # for cell, spiketimes in data.spikes.items():
     #     if cell.startswith('Spiny'):
