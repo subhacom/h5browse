0f381866f4d5f4cb2da2a36a0be878434ab3a4a0
Subhasis <ray.subhasis@gmail.com>
2013-01-17 19:22:37 +0530

    UPdated analyze_stimulus_response to plot spikes in colormap. Refined cell selection.

--- analyze_stimulus_response.py
+++ analyze_stimulus_response.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Wed Jan  2 10:07:34 2013 (+0530)
 # Version: 
-# Last-Updated: Wed Jan 16 18:32:22 2013 (+0530)
+# Last-Updated: Thu Jan 17 19:22:02 2013 (+0530)
 #           By: subha
-#     Update #: 550
+#     Update #: 667
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -71,7 +71,7 @@ def get_cells_responding_to_bg(data):
         #     plotmarker = 'b+'
     #     plt.plot(sp, np.ones(len(sp))*(idx+1), plotmarker)
     # plt.show()
-    print set(data.bg_cells) - set(goodstimcells)
+    # print set(data.bg_cells) - set(goodstimcells)
     return goodstimcells
     #     if 0 in outofturn.shape:
     #         continue
@@ -83,14 +83,14 @@ def test_get_cells_responding_to_bg():
     for fname in fname_stim_dict.keys():
         get_cells_responding_to_bg(TraubData(fname))
 
-def stimresponse(datalist, cells):
+def stimresponse(datalist, cells, figlabel='1'):
     colormap = plt.cm.jet
     plt.close()
     ax = None
     axlist = []
     # Go through each data file, pick up the stimulated cells
-    fig1 = plt.figure(1)
-    fig2 = plt.figure(2)
+    fig1 = plt.figure(figlabel)
+    # fig2 = plt.figure(2)
     for di, data in enumerate(datalist):
         if isinstance(cells, str):
             cells = set([cell for cell in data.spikes.keys() if cell.startswith(cells)])
@@ -104,32 +104,59 @@ def stimresponse(datalist, cells):
         inputcount_cell_map = defaultdict(list)
         for cell, count in inputcounts.items():
             inputcount_cell_map[count].append(cell)
-        ci = 1
         ax = fig1.add_subplot(len(datalist), 1, di+1)
-        ax2 =fig2.add_subplot(111)
+        # ax.set_title(data.fdata.filename)
+        # ax2 =fig2.add_subplot(len(datalist), 1, di+1)
+        # ax2.set_title(data.fdata.filename)
+        norm = mpl.colors.Normalize(vmin=1, vmax=max(inputcount_cell_map.keys()))
+        mappable = mpl.cm.ScalarMappable(norm=norm, cmap=colormap)
+        carray = np.arange(1, 10)
+        mappable.set_array(carray)
         pop_ibi = data.get_pop_ibi(cells, maxisi=30e-3)['pop_ibi']
-        print 'pop-ibi', pop_ibi
+        ci = 1 # index of the cell in the raster plot (used as Y)
+        cellcount = 0 # (total number of cells)
         for count in sorted(inputcount_cell_map.keys()):
+            print count, len(inputcount_cell_map[count])
             for cell in inputcount_cell_map[count]:
                 pre = set(data.presynaptic(cell)).intersection(stimulated_cells)
                 assert(count == len(pre))
                 spikes = data.spikes[cell]
-                ax2.plot(spikes, np.ones(len(spikes)) * ci, 'bx')
+                # ax2.plot(spikes, np.ones(len(spikes)) * ci, 'bx')
                 nonpopspikes = []
                 for ibi in zip(*pop_ibi):
-                    print 'IBI:', ibi
-                    nonpopspikes.append(spikes[(spikes >= ibi[0]) & (spikes < ibi[1])].copy())
+                    # print 'IBI:', ibi
+                    nonpopspikes.append(spikes[(spikes >= ibi[0]) & (spikes < ibi[1])])
                 # print 'Number of bunches in pop-ibi', len(nonpopspikes)
                 # print nonpopspikes
                 spikes = np.concatenate(nonpopspikes)
-                ax.plot(spikes, np.ones(len(spikes)) * ci, 'rx')                
-                ci += 1
-        ax.bar(pop_ibi[0], height=np.ones(len(pop_ibi[0]))*ci, width=np.array(pop_ibi[1])-np.array(pop_ibi[0]), alpha=0.5)
-    plt.show()
-    plt.close()
+                peristim_spikes = np.concatenate([spikes[(spikes > t) & (spikes < t + 50e-3)] for t in data.get_bgstim_times()])
+                if len(np.nonzero(peristim_spikes)[0]) >= len(data.get_bgstim_times()) * 0.5:
+                    ax.plot(spikes, np.ones(len(spikes)) * ci, ',', mew=0.0, color=mappable.to_rgba(count))                
+                    ci += 1
+                cellcount += 1
+        print 'number of cells', ci
+        ax.plot(data.get_bgstim_times(), np.zeros(len(data.get_bgstim_times())), '^')
+        # ax.bar(pop_ibi[0], height=np.ones(len(pop_ibi[0]))*ci, width=np.array(pop_ibi[1])-np.array(pop_ibi[0]), alpha=0.5)
+        ylim = ax.get_ylim()
+        dy = 1
+        if ylim[1] - ylim[0] > 10:
+            dy = (ylim[1] - ylim[0])/5
+        yticks = range(int(ylim[0]), int(ylim[1]), int(dy))
+        ax.set_yticks(yticks)
+        # ax.patch.set_facecolor('black')
+        ylabels = ax.set_yticklabels([str(label) for label in yticks])
+        # print 'ylim', ax.get_ylim()
+        cbar = plt.colorbar(mappable)
+        ticks = range(carray[0], carray[-1])
+        cbar.set_ticks(ticks)
+        cbar.set_ticklabels(['%d' % (t) for t in ticks])
+    return fig1
+    # plt.show()
+    # plt.close()
 
 def test_stim_response():
-    datalist = [TraubData(fname) for fname in random.sample(fname_stim_dict.keys(), 2)]
+    # datalist = [TraubData(fname) for fname in random.sample(fname_stim_dict.keys(), 2)]
+    datalist = [TraubData(fname) for fname in fname_stim_dict.keys()]
     stimresponse(datalist, 'SpinyStellate')
 
 
@@ -140,7 +167,7 @@ def check_stimulus_response(datalist, cells):
     axlist = []
     rows = len(datalist)
     for dataindex, data in enumerate(datalist):
-        print data.fdata.filename, data.cellcounts.DeepBasket
+        # print data.fdata.filename, data.cellcounts.DeepBasket
         # Select the cells of celltype
         if isinstance(cells, str):
             cells = [cell for cell in data.spikes.keys() if cell.startswith(cells)]
@@ -152,7 +179,7 @@ def check_stimulus_response(datalist, cells):
         stimcells = [cell for cell in stimcells if len(data.spikes[cell]) < 2*len(stimtimes)]
         stims = np.concatenate([data.spikes[cell] for cell in stimcells if len(data.spikes[cell]) > 0])
         filtered = np.concatenate([stims[(stims > start+50e-3) & (stims < end-50e-3)].copy() for start, end in zip(popibi[0], popibi[1])])    
-        print filtered
+        # print filtered
         stims = filtered
         postcells = [post for pre in stimcells for post in data.postsynaptic(pre) if post in cells]
         inputcounts = defaultdict(list)
@@ -160,7 +187,7 @@ def check_stimulus_response(datalist, cells):
             indices = np.char.startswith(data.synapse['dest'], cell+'/')
             precells = [row[0] for row in np.char.split(data.synapse['source'][indices], '/') if row[0] in stimcells]
             inputcounts[len(precells)].append(cell)
-        print inputcounts        
+        # print inputcounts        
         ax = fig.add_subplot(rows, 1, dataindex+1, sharex=ax)
         # ax.set_title('%s: %d' % (data.fdata.filename, data.cellcounts.DeepBasket))
         axlist.append(ax)
@@ -176,7 +203,7 @@ def check_stimulus_response(datalist, cells):
                 peristim_spikes = np.concatenate([spikes[np.nonzero((spikes > t) & (spikes < t + 50e-3))[0]] for t in stims])
                 if len(np.nonzero(peristim_spikes)[0]) < 10:
                     continue
-                print len(np.nonzero(peristim_spikes)[0])
+                # print len(np.nonzero(peristim_spikes)[0])
                 ax.plot(peristim_spikes, np.ones(len(peristim_spikes)) * cellindex, 'x', color=mappable.to_rgba(count))
                 cellindex += 1
         st = stimtimes[stimtimes > 1.0].copy()    
@@ -213,134 +240,40 @@ filenames = [
 	/data/subha/rsync_ghevar_cortical_data_clone/2012_11_18/data_20121118_132702_5610.h5 5"""
 
 fname_stim_dict = {
-    '/data/subha/rsync_ghevar_cortical_data_clone/2012_12_08/data_20121208_105807_15611.h5': 15,
-    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_30/data_20121130_083256_12326.h5': 10,
-    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_22/data_20121122_145449_8016.h5': 5,
-    '/data/subha/rsync_ghevar_cortical_data_clone/2012_12_05/data_20121205_165444_16910.h5': 10,
-    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_20/data_20121120_090612_6590.h5': 5,
-    '/data/subha/rsync_ghevar_cortical_data_clone/2012_12_21/data_20121221_151958_9665.h5': 15,
-    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_26/data_20121126_092942_10181.h5': 10,
+    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_14/data_20121114_091030_2716.h5': 5,
     '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_16/data_20121116_091100_3774.h5': 5,
+    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_18/data_20121118_132702_5610.h5': 5,
+    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_20/data_20121120_090612_6590.h5': 5,
+    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_22/data_20121122_145449_8016.h5': 5,
     '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_24/data_20121124_162657_9363.h5': 10,
+    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_26/data_20121126_092942_10181.h5': 10,
     '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_28/data_20121128_092639_11369.h5': 10,
+    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_30/data_20121130_083256_12326.h5': 10,
+    '/data/subha/rsync_ghevar_cortical_data_clone/2012_12_05/data_20121205_165444_16910.h5': 10,
+    '/data/subha/rsync_ghevar_cortical_data_clone/2012_12_08/data_20121208_105807_15611.h5': 15,
     '/data/subha/rsync_ghevar_cortical_data_clone/2012_12_11/data_20121211_103522_12008.h5': 15,
     '/data/subha/rsync_ghevar_cortical_data_clone/2012_12_14/data_20121214_095338_17603.h5': 15,
-    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_14/data_20121114_091030_2716.h5': 5,
     '/data/subha/rsync_ghevar_cortical_data_clone/2012_12_18/data_20121218_090355_29114.h5': 15,
-    '/data/subha/rsync_ghevar_cortical_data_clone/2012_11_18/data_20121118_132702_5610.h5': 5,
+    '/data/subha/rsync_ghevar_cortical_data_clone/2012_12_21/data_20121221_151958_9665.h5': 15,
     }
 
-def on_draw(event):
-    """Auto-wraps all text objects in a figure at draw-time
-
-    Lifted from
-    http://stackoverflow.com/questions/8802918/my-matplotlib-title-gets-cropped which lifted it from
-
-    http://stackoverflow.com/questions/4018860/text-box-in-matplotlib/4056853#4056853
-    """
-    import matplotlib as mpl
-    fig = event.canvas.figure
-    # This does not work as we do not have axes associated with the text at top level
-    # for artist in fig.get_children():
-    #     # If it's a text artist, wrap it...
-    #     if isinstance(artist, mpl.text.Text):
-    #         autowrap_text(artist, event.renderer)
-    # Cycle through all artists in all the axes in the figure
-    for ax in fig.axes:
-        for artist in ax.get_children():
-            # If it's a text artist, wrap it...
-            if isinstance(artist, mpl.text.Text):
-                autowrap_text(artist, event.renderer)
-
-    # Temporarily disconnect any callbacks to the draw event...
-    # (To avoid recursion)
-    func_handles = fig.canvas.callbacks.callbacks[event.name]
-    fig.canvas.callbacks.callbacks[event.name] = {}
-    # Re-draw the figure..
-    fig.canvas.draw()
-    # Reset the draw event callbacks
-    fig.canvas.callbacks.callbacks[event.name] = func_handles
-
-import textwrap
-
-def autowrap_text(textobj, renderer):
-    """Wraps the given matplotlib text object so that it exceed the boundaries
-    of the axis it is plotted in.
-
-    Lifted from
-    http://stackoverflow.com/questions/8802918/my-matplotlib-title-gets-cropped which lifted it from
-
-    http://stackoverflow.com/questions/4018860/text-box-in-matplotlib/4056853#4056853
-    """
-    # Get the starting position of the text in pixels...
-    x0, y0 = textobj.get_transform().transform(textobj.get_position())
-    # Get the extents of the current axis in pixels...
-    clip = textobj.get_axes().get_window_extent()
-    # Set the text to rotate about the left edge (doesn't make sense otherwise)
-    textobj.set_rotation_mode('anchor')
-
-    # Get the amount of space in the direction of rotation to the left and 
-    # right of x0, y0 (left and right are relative to the rotation, as well)
-    rotation = textobj.get_rotation()
-    right_space = min_dist_inside((x0, y0), rotation, clip)
-    left_space = min_dist_inside((x0, y0), rotation - 180, clip)
-
-    # Use either the left or right distance depending on the horiz alignment.
-    alignment = textobj.get_horizontalalignment()
-    if alignment is 'left':
-        new_width = right_space 
-    elif alignment is 'right':
-        new_width = left_space
-    else:
-        new_width = 2 * min(left_space, right_space)
-
-    # Estimate the width of the new size in characters...
-    aspect_ratio = 0.5 # This varies with the font!! 
-    fontsize = textobj.get_size()
-    pixels_per_char = aspect_ratio * renderer.points_to_pixels(fontsize)
-
-    # If wrap_width is < 1, just make it 1 character
-    wrap_width = max(1, new_width // pixels_per_char)
-    try:
-        wrapped_text = textwrap.fill(textobj.get_text(), wrap_width)
-    except TypeError:
-        # This appears to be a single word
-        wrapped_text = textobj.get_text()
-    textobj.set_text(wrapped_text)
-
-def min_dist_inside(point, rotation, box):
-    """Gets the space in a given direction from "point" to the boundaries of
-    "box" (where box is an object with x0, y0, x1, & y1 attributes, point is a
-    tuple of x,y, and rotation is the angle in degrees)
-
-    Lifted from
-    http://stackoverflow.com/questions/8802918/my-matplotlib-title-gets-cropped which lifted it from
-
-    http://stackoverflow.com/questions/4018860/text-box-in-matplotlib/4056853#4056853
-    """
-    from math import sin, cos, radians
-    x0, y0 = point
-    rotation = radians(rotation)
-    distances = []
-    threshold = 0.0001 
-    if cos(rotation) > threshold: 
-        # Intersects the right axis
-        distances.append((box.x1 - x0) / cos(rotation))
-    if cos(rotation) < -threshold: 
-        # Intersects the left axis
-        distances.append((box.x0 - x0) / cos(rotation))
-    if sin(rotation) > threshold: 
-        # Intersects the top axis
-        distances.append((box.y1 - y0) / sin(rotation))
-    if sin(rotation) < -threshold: 
-        # Intersects the bottom axis
-        distances.append((box.y0 - y0) / sin(rotation))
-    return min(distances)
-
-def plot_stim_response():
-    stim_fname = defaultdict(list)
+def plot_stimcount_response():
+    """Plot stimulus response for each stimulus count"""
+    stim_fname = defaultdict(list) # Stimulus count -> data-list map
     for k, v in fname_stim_dict.items():
         stim_fname[v].append(k)
+    # Now we do the plotting for each stimulus count in increasing order
+    for key in sorted(stim_fname.keys()):
+        fig = stimresponse([TraubData(fname) for fname in stim_fname[key]], 'SpinyStellate', 'Stimulated TCR: %d' % (key))
+        fig.suptitle('Stimulated TCR: %d' % (key))
+        plt.show()
+
+def plot_stim_response():
+    """Plot stimulus response for each stimulus count"""
+    stim_fname = defaultdict(list) # Stimulus count -> data-list map
+    for k, v in fname_stim_dict.items():
+        stim_fname[v].append(k)
+    # Now we do the plotting for each stimulus count in increasing order
     for key in sorted(stim_fname.keys()):
         check_stimulus_response([TraubData(fname) for fname in stim_fname[key]], 'SpinyStellate')
         
@@ -410,7 +343,8 @@ def plot_spikeraster_by_stim_count(timerange=(10.0, 12.0)):
     
         
 if __name__ == '__main__':
-    test_stim_response()
+    plot_stimcount_response()
+    # test_stim_response()
     # test_get_cells_responding_to_bg()
     # plot_stim_response()
     # plot_spikeraster_by_stim_count()
