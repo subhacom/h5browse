91eb3acc040527dc35015481411ae6a6a291d14b
Subhasis Ray <ray.subhasis@gmail.com>
2015-07-20 23:06:18 -0400

    Starting on a clean slate.

--- makegif.py
+++
@@ -1,9 +0,0 @@
-
-def make_gif():
-    with open('frames.txt', 'w') as fd:
-        for num in range(0, 800, 10):
-            fname = 'frame_%06d.png' % (num)
-            fd.write('%s\n' % (fname))
-
-if __name__ == '__main__':
-    make_gif()
