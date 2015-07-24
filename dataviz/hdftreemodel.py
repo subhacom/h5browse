1056aee2ba9c9cc0c3763cf86abcaabd21897712
Subhasis Ray <ray.subhasis@gmail.com>
2015-07-24 01:03:57 -0400

    Functional tree model - can display HDF5 tree.

---
+++ dataviz/hdftreemodel.py
@@ -0,0 +1,227 @@
+# hdftreemodel.py --- 
+# 
+# Filename: hdftreemodel.py
+# Description: 
+# Author: subha
+# Maintainer: 
+# Created: Thu Jul 23 22:07:53 2015 (-0400)
+# Version: 
+# Last-Updated: Fri Jul 24 01:02:01 2015 (-0400)
+#           By: subha
+#     Update #: 266
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# 
+# 
+# 
+
+# Change log:
+# 
+# 
+# 
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# 
+# 
+
+# Code:
+
+"""This code borrows heavily from the PyQt5 example:
+editabletreemodel.py
+
+- Subhasis Ray, Thu Jul 23 23:40:39 EDT 2015
+
+"""
+
+import h5py as h5
+from PyQt5.QtCore import (QAbstractItemModel, QItemSelectionModel, QModelIndex, Qt)
+
+class RootItem(object):
+    def __init__(self, parent=None):
+        self.parentItem = parent
+        self.children = []
+        
+    def child(self, row):
+        if row < 0 or row >= len(self.children):
+            return None
+        return self.children[row]
+            
+    def childCount(self):
+        return len(self.children)
+
+    def childNumber(self):
+        return 0
+
+    def columnCount(self):
+        return 1
+
+    def data(self, column, role=Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            return 'Files'
+        elif role == Qt.ToolTipRole:
+            return 'List of open files'
+
+    def setData(self, column, value):
+        return False
+
+    def parent(self):
+        return None
+
+    def appendChild(self, childItem):
+        self.children.append(childItem)
+
+    def removeChild(self, position):
+        if position < 0 or position > len(self.children):
+            return False
+        child = self.children.pop(position)
+        return True
+
+
+class HDFTreeItem(object):
+    def __init__(self, data, parent=None):
+        self.h5node = data
+        self.parentItem = parent
+        self.children = []
+        if isinstance(self.h5node, h5.Group):
+            self.children = [HDFTreeItem(child, self) for child in self.h5node.values()]        
+        
+    def child(self, row):
+        return self.children[row]
+            
+    def childCount(self):
+        if isinstance(self.h5node, h5.Group):
+            return len(self.h5node)
+        return 0
+
+    def childNumber(self):
+        return self.parentItem.children.index(self)
+
+    def columnCount(self):
+        return 1
+
+    def data(self, column, role=Qt.DisplayRole):
+        if self.h5node == None:
+            return ''
+        if role == Qt.DisplayRole:
+            if isinstance(self.h5node, h5.File):
+                return '/'
+            return self.h5node.name.rsplit('/')[-1]
+        elif role == Qt.ToolTipRole:
+            if isinstance(self.h5node, h5.File):
+                return self.h5node.filename
+            return '{} : {}'.format(self.h5node.name,
+                                    self.h5node.__class__.__name__)
+
+    def setData(self, column, value):
+        if column != 0:
+            return False
+        self.h5node = value
+
+    def parent(self):
+        return self.parentItem
+            
+
+class HDFTreeModel(QAbstractItemModel):
+    def __init__(self, headers, parent=None):
+        super(HDFTreeModel, self).__init__(parent)
+        rootData = [header for header in headers]
+        self.rootItem = RootItem() 
+        
+    def columnCount(self, parent=QModelIndex()):
+        return self.rootItem.columnCount()
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        item = self.getItem(index)
+        return item.data(index.column(), role)
+
+    def getItem(self, index):
+        if index.isValid():
+            item = index.internalPointer()
+            if item:
+                return item
+        return self.rootItem
+
+    def flags(self, index):
+        if not index.isValid():
+            return 0
+        return Qt.ItemIsEnabled |Qt.ItemIsSelectable
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if orientation == Qt.Horizontal:
+            return self.rootItem.data(section, role)
+        return None
+
+    def index(self, row, column, parent=QModelIndex()):
+        if parent.isValid() and parent.column()!= 0:
+            return QModelIndex()
+
+        parentItem = self.getItem(parent)
+        childItem = parentItem.child(row)
+        if childItem:
+            return self.createIndex(row, column, childItem)
+        else:
+            return QModelIndex()
+
+    def parent(self, index):
+        if not index.isValid():
+            return QModelIndex()
+        
+        childItem = self.getItem(index)
+        parentItem = childItem.parent()
+
+        if parentItem == self.rootItem:
+            return QModelIndex()
+
+        return self.createIndex(parentItem.childNumber(), 0, parentItem)
+
+    def rowCount(self, parent=QModelIndex()):
+        parentItem = self.getItem(parent)
+        return parentItem.childCount()
+
+    def openFile(self, path):
+        fd = h5.File(path, 'r')
+        fileItem = HDFTreeItem(fd, self.rootItem)
+        self.rootItem.appendChild(fileItem)
+
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QTreeView, QWidget)
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    widget = QWidget()
+    layout = QVBoxLayout(widget)
+    view = QTreeView(widget)
+    layout.addWidget(view)
+    window.setCentralWidget(widget)
+    model = HDFTreeModel([])
+    model.openFile('poolroom.h5')
+    view.setModel(model)
+    window.show()
+    sys.exit(app.exec_())
+    
+
+
+# 
+# hdftreemodel.py ends here
---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='dataviz',
+      version=version,
+      description="dataviz is a utility for viewing HDF5 datasets.",
+      long_description="""\ 
+dataviz is a utility for viewing HDF5 datasets.
+
+It was developed for analyzing neuronal simulation data saved in a
+custom format. But the interface is generic enough to display simple
+HDF5 files. The tools are totally for neuronal data analysis. But the
+plan is to make it a more general tool.
+
+This is a rewrite of the old code. Replacing PyQwt with PyQtGraph (or
+some other module).
+
+It uses  Python 3.x, numpy, PyQt5, h5py, PyQtGraph.
+
+Written from scratch in Python 3.
+
+""",
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Development Status :: 1 - Planning', 
+                   'Environment :: X11 Applications :: Qt',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                   'Intended Audience :: Science/Research',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Topic :: Scientific/Engineering :: Visualization',
+               ],
+      keywords='hdf5 data plotting analysis',
+      author='Subhasis Ray',
+      author_email='lastname dot firstname at gmail dot com',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'numpy',
+          'h5py',
+          'PyQt5',
+          'PyQtGraph',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
