c09bcb8f6732dc35d886a80fb6264bf2a177eb1b
Subhasis Ray <ray.subhasis@gmail.com>
2015-08-24 12:10:49 -0400

    Fixed plotting.
    Now data can be selected along any axis and plotted. For 2D data row and
    columns were confusing. Now data in 'rows' means each row contains a
    series (i.e., select the row to plot) and data in 'columns' means each
    column contains a series (i.e. select the column to plot).

--- dataviz/datasetplot.py
+++ dataviz/datasetplot.py
@@ -7,9 +7,9 @@
 # Created: Fri Aug 21 17:21:21 2015 (-0400)
 # Version: 
 # Package-Requires: ()
-# Last-Updated: Sun Aug 23 06:07:09 2015 (-0400)
+# Last-Updated: Mon Aug 24 12:09:53 2015 (-0400)
 #           By: subha
-#     Update #: 560
+#     Update #: 813
 # URL: 
 # Doc URL: 
 # Keywords: 
@@ -44,8 +44,9 @@
 
 # Code:
 
+import numpy as np
 from collections import defaultdict
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import (Qt, pyqtSignal)
 import pyqtgraph as pg
 from pyqtgraph import parametertree as ptree
 import numpy as np
@@ -56,10 +57,17 @@ class DatasetPlotParamTree(ptree.ParameterTree):
     to various dataset types.
 
     """
+    sigUpdateData = pyqtSignal() 
+    
     def __init__(self, parent=None, showHeader=True, dataset=None):
         super().__init__(parent=parent,
                          showHeader=showHeader)        
         self.dataset = dataset
+        self.updatePlotData = ptree.Parameter.create(name='updatePlotData',
+                                                     title='Update plot data',
+                                                     type='action')
+        self.addParameters(self.updatePlotData)
+        self.updatePlotData.sigActivated.connect(self.sigUpdateData)
 
     def setDataset(self, dataset):
         self.dataset = dataset
@@ -87,7 +95,15 @@ class OneDPlotParamTree(DatasetPlotParamTree):
         self.addParameters(self.dataSources)            
 
     def getXY(self):
-        return range(self.dataset.shape[0]), self.dataset
+        if self.xsource.value() == 'index':
+            xdata = range(self.dataset.shape[0])
+        else:
+            xdata = self.dataset
+        if self.ysource.value() == 'index':
+            ydata  = range(self.dataset.shape[0])
+        else:
+            ydata = self.dataset
+        return xdata, ydata
         
 class CompoundPlotParamTree(OneDPlotParamTree):
     """Compound dataset is tabulated data. One can choose row index or a
@@ -109,6 +125,7 @@ class CompoundPlotParamTree(OneDPlotParamTree):
     def setDataset(self, dataset):
         self.dataset = dataset
         dataSources = ['index'] + list(dataset.dtype.names)
+        print('compund data sources', dataSources)
         self.xsource.setLimits(dataSources)
         self.ysource.setLimits(dataSources)
         self.ysource.setValue(dataSources[1])
@@ -133,7 +150,7 @@ class TwoDPlotParamTree(DatasetPlotParamTree):
                          showHeader=showHeader,
                          dataset=dataset)
         self.dataDim = ptree.Parameter.create(name='datadim',
-                                              title='Data along dimension:',
+                                              title='Data in:',
                                               type='list',
                                               values=['rows', 'columns'],
                                               value='columns')
@@ -162,22 +179,39 @@ class TwoDPlotParamTree(DatasetPlotParamTree):
         else:
             dataSources = ['index'] + list(range(dataset.shape[0]))
         self.xsource.setLimits(dataSources)
+        self.xsource.setValue(dataSources[0])
         self.ysource.setLimits(dataSources)
         self.ysource.setValue(dataSources[1])
 
     def dataDimChanged(self):
         if self.dataDim.value() == 'columns':
-            headerData = [str(ii) for ii in range(self.dataset.shape[1])]
+            headerData = list(range(self.dataset.shape[1]))
         else:
-            headerData = [str(ii) for ii in range(self.dataset.shape[0])]
+            headerData = list(range(self.dataset.shape[0]))
+        print('TwoDPlotParamTree: dataset', self.dataset)
         dataSources = ['index'] + headerData
+        print('headerData', dataSources)
+        self.xsource.setLimits(dataSources)
+        self.xsource.setValue(dataSources[0])
+        self.ysource.setLimits(dataSources)
+        self.ysource.setValue(dataSources[1])
 
     def getXY(self):
+        print('2D', self, self.dataset)
         ds = self.dataset
         xdim = self.xsource.value()
         ydim = self.ysource.value()
-        print(ydim, type(ydim))
-        if self.dataDim.value() == 'row':
+        print('####', xdim, ydim, self.dataDim.value())
+        if self.dataDim.value() == 'rows':
+            if xdim == 'index':
+                xdata = range(ds.shape[1])
+            else:
+                xdata = ds[xdim,:]
+            if ydim == 'index':
+                ydata = range(ds.shape[1])
+            else:
+                ydata = ds[ydim, :]
+        elif self.dataDim.value() == 'columns':
             if xdim == 'index':
                 xdata = range(ds.shape[0])
             else:
@@ -185,18 +219,9 @@ class TwoDPlotParamTree(DatasetPlotParamTree):
             if ydim == 'index':
                 ydata = range(ds.shape[0])
             else:
-                ydata = ds[:, ydim]
-        if self.dataDim.value() == 'columns':
-            if xdim == 'index':
-                xdata = range(ds.shape[1])
-            else:
-                xdata = ds[xdim, :]
-            if ydim == 'index':
-                ydata = range(ds.shape[0])
-            else:
-                ydata = ds[ydim, :]
-        
+                ydata = ds[:, ydim]        
         return xdata, ydata
+
 
 class NDPlotParamTree(DatasetPlotParamTree):
     """Class to allow the user to choose parameters for plotting."""
@@ -227,7 +252,8 @@ class NDPlotParamTree(DatasetPlotParamTree):
         self.dataset = dataset
         ndim = len(dataset.shape)
         self.dataDim.setLimits(range(len(dataset.shape)))
-        dimChoices = [{'name': 'Dim{}'.format(ii),
+        dimChoices = [{'name': 'dim{}'.format(ii),
+                       'title': str(ii),
                        'type': 'list',
                        'values': ['index']+list(range(dataset.shape[ii])),
                        'value': 0} for ii in range(1, ndim)]
@@ -239,7 +265,8 @@ class NDPlotParamTree(DatasetPlotParamTree):
     def dataDimChanged(self):
         dims = list(range(len(self.dataset.shape)))
         dims.pop(self.dataDim.value())
-        dimChoices = [{'name': 'Dim{}'.format(ii),
+        dimChoices = [{'name': 'dim{}'.format(ii),
+                       'title': str(ii),
                        'type': 'list',
                        'values': ['index']+list(range(self.dataset.shape[ii])),
                        'value': 'index'} for ii in dims]
@@ -251,6 +278,8 @@ class NDPlotParamTree(DatasetPlotParamTree):
         self.ysource.addChildren(dimChoices)
 
     def getXY(self):
+        print('NDPlotParamTree: xsource: {}, ysource: {}'.format(
+            self.xsource.value(), self.ysource.value()))
         dims = range(len(self.dataset.shape))
         xindex = []
         yindex = []
@@ -260,11 +289,11 @@ class NDPlotParamTree(DatasetPlotParamTree):
                 xindex.append(slice(self.dataset.shape[dim]))
                 yindex.append(slice(self.dataset.shape[dim]))
                 continue
-            xparam = self.xsource.param('Dim{}'.format(dim))
+            xparam = self.xsource.param('dim{}'.format(dim))
             xindex.append(xparam.value())
-            yparam = self.ysource.param('Dim{}'.format(dim))
+            yparam = self.ysource.param('dim{}'.format(dim))
             yindex.append(yparam.value())
-        print('xindex', xindex, 'yindex', yindex)
+        # data = np.asarray(self.dataset)
         if 'index' in  xindex:
             xdata = range(self.dataset.shape[dataDim])
         else:
@@ -272,10 +301,12 @@ class NDPlotParamTree(DatasetPlotParamTree):
             # xdata = self.dataset[xindex] 
             # Therefore using hack
             xdata = eval('self.dataset{}'.format(xindex))
+            # xdata = data[xindex]
         if 'index' in  yindex:
             ydata = range(self.dataset.shape[dataDim])
         else:
             ydata = eval('self.dataset{}'.format(yindex))
+            # ydata = data[yindex]
         return (xdata, ydata)
         
         
@@ -289,12 +320,12 @@ class DatasetPlot(pg.PlotWidget):
     def __init__(self, parent=None, background='default', **kwargs):
         pg.PlotWidget.__init__(self, parent=parent, background=background, **kwargs)
         self.name = ''        
-        self.params = defaultdict(list)
+        self.plotToParams = {}
+        self.paramsToPlots = {}
 
     def plotLine(self, dataset):
         dtype = datasetType(dataset)
         if dtype == 'scalar':
-            print('{} is a scalar - nothing to plot'.format(dataset))
             return
         if dtype == 'compound':
             params = CompoundPlotParamTree(dataset=dataset)
@@ -305,6 +336,7 @@ class DatasetPlot(pg.PlotWidget):
         else: # dtype == 'nd':
             params = NDPlotParamTree(dataset=dataset)
         params.name = dataset.name
+        params.sigUpdateData.connect(self.updatePlotData)
         #A better idea may be to keep all the params under a single tree
         # self.params[dataset].append(params)
         params.show()
@@ -315,21 +347,27 @@ class DatasetPlot(pg.PlotWidget):
         #     xdata = np.arange(len(dataset)) * float(sched['simtime']) / len(dataset)
         # except KeyError:
         #     xdata = np.arange(len(dataset))
-        plot = self.plot(xdata, ydata)
-        print('Plot=', plot)
+        plotDataItem = self.plot(xdata, ydata)
+        self.plotToParams[plotDataItem] = params
+        self.paramsToPlots[params] = plotDataItem
+        # print('Plot=', plot)
         self.name = '{}:{}'.format(dataset.file.filename,
                                    dataset.name)
-        return plot, params
+        return plotDataItem, params
 
-if __name__ == '__main__':
-    import h5py as h5
-    from hdfdatasetmodel import (CompoundDatasetModel, NDDatasetModel, TwoDDatasetModel, OneDDatasetModel, create_default_model)
-    import sys
-    from PyQt5.QtWidgets import (QApplication, QMainWindow, QHBoxLayout, QTreeView, QWidget, QTableView)
-    app = QApplication(sys.argv)
-    fd = h5.File('poolroom.h5')
-    twodModel = create_default_model(fd['/data/uniform/balls/x'])
-    ndModel = create_default_model(fd['/data/uniform/ndim/data3d'], pos=('*', 1, '*'))
+    def updatePlotData(self):
+        if self.sender() != 0:
+            x, y = self.sender().getXY()
+            plotDataItem = self.paramsToPlots[self.sender()]
+            plotDataItem.setData(x, y)
+
+
+import h5py as h5
+from hdfdatasetmodel import (CompoundDatasetModel, NDDatasetModel, TwoDDatasetModel, OneDDatasetModel, create_default_model)
+import sys
+from PyQt5.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QWidget)
+
+def testDatasetPlotParams(fd):
     widget = QWidget()
     widget.setLayout(QHBoxLayout())
     onedParams = OneDPlotParamTree(dataset=fd['/data/event/balls/hit/ball_0_9ba91cb6163611e5899524fd526610e7'])
@@ -341,7 +379,37 @@ if __name__ == '__main__':
     ndParams = NDPlotParamTree(dataset=fd['/data/uniform/ndim/data3d'])
     widget.layout().addWidget(ndParams)
     widget.show()
-    sys.exit(app.exec_())
+    return widget
+
+
+def testDatasetPlot(fd):
+    widget = QWidget()
+    widget.setLayout(QGridLayout())
+    oneDimPlot = DatasetPlot()
+    plot, params = oneDimPlot.plotLine(fd['/data/event/balls/hit/ball_0_9ba91cb6163611e5899524fd526610e7'])
+    widget.layout().addWidget(params, 0, 0)
+    widget.layout().addWidget(oneDimPlot, 1, 0)        
+    compoundPlot = DatasetPlot()
+    plot, params = compoundPlot.plotLine(fd['/data/static/tables/dimensions'])
+    widget.layout().addWidget(params, 0, 1)
+    widget.layout().addWidget(compoundPlot, 1, 1)        
+    twoDimPlot = DatasetPlot()
+    plot, params = twoDimPlot.plotLine(fd['/data/uniform/balls/x'])
+    widget.layout().addWidget(params, 0, 2)
+    widget.layout().addWidget(twoDimPlot, 1, 2)
+    nDimPlot = DatasetPlot()
+    plot, params = nDimPlot.plotLine(fd['/data/uniform/ndim/data3d'])
+    widget.layout().addWidget(params, 0, 3)
+    widget.layout().addWidget(nDimPlot, 1, 3)
+    widget.show()
+    return widget
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    with h5.File('poolroom.h5', 'r') as fd:
+        dparamw = testDatasetPlotParams(fd)
+        dplotw = testDatasetPlot(fd)
+        sys.exit(app.exec_())
 
 
 # 
